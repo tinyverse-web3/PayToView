92f3bc970b4ddd0c75aa712f1151bb046492041d
Hugh <ygz14835187@163.com>
2023-10-16 18:30:01 +0800

    add locals

--- miniapp/src/main.tsx
+++ miniapp/src/main.tsx
@@ -1,8 +1,6 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import '@/locales';
+import App from '@/App.tsx';
+import '@/style/index.css';
 
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
