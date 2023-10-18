d514a47dd8d88ba8476a747f0292f34ec5bf51f8
Hugh <ygz14835187@163.com>
2023-10-18 15:56:03 +0800

    miniapp test MainButton

--- miniapp/src/App.tsx
+++ miniapp/src/App.tsx
@@ -1,16 +1,22 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { router } from '@/router';
 import { RouterProvider } from 'react-router-dom';
-import { WebAppProvider } from '@vkruglikov/react-telegram-web-app';
+import {
+  WebAppProvider,
+  BackButton,
+  MainButton,
+} from '@vkruglikov/react-telegram-web-app';
 function App() {
   return (
     <WebAppProvider
       options={{
         smoothButtonsTransition: true,
       }}>
+      <BackButton />
       <ChakraProvider>
         <RouterProvider router={router}></RouterProvider>
       </ChakraProvider>
+      <MainButton text="test"/>
     </WebAppProvider>
   );
 }
