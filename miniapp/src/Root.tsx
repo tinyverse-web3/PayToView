07bfa245ab9f3b79f26f65ed229d07e0161ecd1d
Hugh <ygz14835187@163.com>
2023-10-20 11:27:12 +0800

    miniapp add window attr

--- miniapp/src/Root.tsx
+++ miniapp/src/Root.tsx
@@ -1,12 +1,31 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useSearchParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { Spinner } from '@chakra-ui/react';
+import { useWebApp } from '@vkruglikov/react-telegram-web-app';
 
 export default function Root() {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
+  const [searchParams] = useSearchParams();
+  const user = searchParams.get('user');
+  const webApp = useWebApp();
+
+  const getUserId = async () => {
+    if (!window.Telegram) {
+      return undefined;
+    } else {
+      const webAppUserId =  webApp?.initDataUnsafe?.user?.id;
+      return webAppUserId || user;
+    }
+  }
   const check = () => {
     setLoading(true);
+    const userId = getUserId();
+    if (!userId) {
+      setError(true);
+    } 
+
     setTimeout(() => {
       setLoading(false);
     }, 3000);
