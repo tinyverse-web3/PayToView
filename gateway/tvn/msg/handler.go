1137dd849f246a849fea2de847e011a89b8b686d
softwarecheng <softwarecheng@126.com>
2023-10-21 02:23:28 +0000

    fix

--- gateway/tvn/msg/handler.go
+++ gateway/tvn/msg/handler.go
@@ -110,7 +110,6 @@ func msgProxyReadMailboxHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "application/json")
-		r.ParseForm()
 
 		resp := msgReadMailboxResp{
 			Key:    "",
