40730d3c98c8e07db7eeb14a3b4cfcb07f0af882
jacky <softwarecheng@gmail.com>
2023-11-03 17:53:39 +0800

    fix

--- miniapp/src/components/IpfsImage.tsx
+++ miniapp/src/components/IpfsImage.tsx
@@ -8,6 +8,7 @@ export const IpfsImage = ({ cid, ...rest }: Props) => {
   const { accountInfo } = useAccountStore((state) => state);
   console.log('IpfsImage->accountInfo:', accountInfo);
   console.log('IpfsImage->cid:', cid);
+  accountInfo.publicKey = '04028def32b006d87426b221ca253748ac3545d729a0aa444676cc5ff0e3733c8bacdd25a5d3350a27bb89019b2662a1e219a19f9e3fbf93abfc751936b34a4137'
   const src = useMemo(
     () =>
       `${import.meta.env.VITE_IPFS_GATEWAY_URL}/cat?pubkey=${accountInfo.publicKey
