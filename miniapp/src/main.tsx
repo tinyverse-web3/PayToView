b106ba6f75196161f4547a431f259f1f33bfceb8
jacky <softwarecheng@gmail.com>
2023-11-25 17:38:55 +0800

    add eth wallet connect

--- miniapp/src/main.tsx
+++ miniapp/src/main.tsx
@@ -5,4 +5,48 @@ import 'react-photo-view/dist/react-photo-view.css';
 import '@/style/tailwind.css';
 import '@/style/index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+
+// for eth begin
+import React from "react";
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
+
+import { WagmiConfig } from "wagmi";
+import {
+    mainnet,
+    sepolia
+
+} from "wagmi/chains";
+
+let chains = [
+    mainnet,
+    sepolia
+];
+
+debugger
+const projectId = import.meta.env.VITE_EVM_WALLET_CONNECT_PROJECT_ID || "";
+
+const metadata = {
+    name: "mini paytoview",
+    description: "mini paytoview",
+    url: "https://tinyverse.minipaytoview.com",
+    icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+var wagmiConfig = defaultWagmiConfig({ chains: chains, projectId, metadata });
+createWeb3Modal({ wagmiConfig, projectId, chains: chains });
+
+if (import.meta.env.MODE === "production") {
+    chains = chains.filter(chain => chain !== sepolia);
+}
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <WagmiConfig config={wagmiConfig}>
+            <App />
+        </WagmiConfig>
+    </React.StrictMode>
+);
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+
+// for eth end
