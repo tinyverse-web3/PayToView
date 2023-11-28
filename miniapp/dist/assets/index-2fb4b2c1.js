49dd6d84509196c030adf21981b6306d54b9e4a1
Hugh <ygz14835187@163.com>
2023-11-28 16:52:59 +0800

    miniapp hide earn

--- miniapp/dist/assets/index-2fb4b2c1.js
+++ miniapp/dist/assets/index-2fb4b2c1.js
@@ -1,4 +1,4 @@
-import{n as ve,s as qe,T as O,t as W,a as u,o as Q,R as Nr,p as U,y as ne}from"./index-54ea5a0b.js";import{al as S,am as Ur}from"./index-1795a685.js";/**
+import{n as ve,s as qe,T as O,t as W,a as u,o as Q,R as Nr,p as U,y as ne}from"./index-b1233a47.js";import{al as S,am as Ur}from"./index-b79f25b8.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
--- miniapp/src/pages/index.tsx
+++ miniapp/src/pages/index.tsx
@@ -55,13 +55,13 @@ export default function Index() {
         nav(ROUTE_PATH.PUBLISH);
       },
     },
-    {
-      name: t('pages.earning.icon_title'),
-      icon: '/images/earning.png',
-      onClick: () => {
-        toEarn();
-      },
-    },
+    // {
+    //   name: t('pages.earning.icon_title'),
+    //   icon: '/images/earning.png',
+    //   onClick: () => {
+    //     toEarn();
+    //   },
+    // },
     {
       name: t('pages.published.icon_title'),
       icon: '/images/published.png',
@@ -130,7 +130,7 @@ export default function Index() {
               </div>
             </div>
             <div className=' bg-gray-100 p-4 mb-4 rounded-2xl'>
-              <SimpleGrid columns={2} columnGap='20px'>
+              <SimpleGrid columns={1} columnGap='20px'>
                 <div className='text-center bg-gray-200 p-2 rounded-xl'>
                   <div className='mb-2 text-xs'>{t('pages.index.address')}</div>
                   <div className='flex items-end justify-center'>
@@ -140,7 +140,7 @@ export default function Index() {
                     {/* <span className='text-xs leading-none '>TVS</span> */}
                   </div>
                 </div>
-                <div
+                {/* <div
                   className='text-center bg-gray-200 p-2 rounded-xl'
                   onClick={toEarn}>
                   <div className='mb-2 text-xs'>{t('pages.index.profit')}</div>
@@ -148,7 +148,7 @@ export default function Index() {
                     <span className='mr-2 text-sm font-bold leading-none'>50</span>
                     <span className='text-xs leading-none'>TVS</span>
                   </div>
-                </div>
+                </div> */}
               </SimpleGrid>
             </div>
             <div className='mt-4 mb-4 m-2'>
