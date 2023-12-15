ea427ea56a4256f4b68c4ee30e449e9afb22cf4f
Hugh <ygz14835187@163.com>
2023-12-15 16:49:06 +0800

    miniapp change userid

--- miniapp/src/Root.tsx
+++ miniapp/src/Root.tsx
@@ -31,7 +31,7 @@ export default function Root() {
       }
     } else {
       const webAppUserId = webApp?.initDataUnsafe?.user?.id;
-      return (webAppUserId || user).toString() + '14';
+      return (webAppUserId || user).toString() + '15';
     }
   };
   const loadApp = async () => {
