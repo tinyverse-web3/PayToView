5c4ad3ce931e56feb3367f8fbe6652686f750464
Hugh <ygz14835187@163.com>
2023-12-15 15:47:27 +0800

    miniapp change userid

--- miniapp/dist/assets/index-961a976f.js
+++ miniapp/dist/assets/index-961a976f.js
@@ -1,4 +1,4 @@
-import{n as ve,s as qe,T as O,t as W,a as u,o as Q,R as Nr,p as U,y as ne}from"./index-e1b4e06a.js";import{al as S,am as Ur}from"./index-4ee8c790.js";/**
+import{n as ve,s as qe,T as O,t as W,a as u,o as Q,R as Nr,p as U,y as ne}from"./index-7e6035c9.js";import{al as S,am as Ur}from"./index-5d2c6d5c.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
