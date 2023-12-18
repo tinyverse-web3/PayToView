98bb0010be6ae2263b13517ef4c7a07c66158032
Hugh <ygz14835187@163.com>
2023-12-18 11:17:47 +0800

    miniapp change wasm

--- miniapp/src/Root.tsx
+++ miniapp/src/Root.tsx
@@ -31,7 +31,7 @@ export default function Root() {
       }
     } else {
       const webAppUserId = webApp?.initDataUnsafe?.user?.id;
-      return (webAppUserId || user).toString() + '15';
+      return (webAppUserId || user).toString() + '16';
     }
   };
   const loadApp = async () => {
