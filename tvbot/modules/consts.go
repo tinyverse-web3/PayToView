2618b8a8241e79fda8661edb1e97de09f43ffa22
wqftv <wangqingfeng@tinyverse.space>
2024-01-05 10:24:00 +0800

    modify the tvbot

--- tvbot/modules/consts.go
+++ tvbot/modules/consts.go
@@ -1,6 +1,7 @@
 package modules
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -23,12 +24,12 @@ var Client = http.Client{Timeout: time.Second * 10}
 var CNT = []string{"locks", "flood", "filters", "get", "notes", "saved", "adminlist", "info", "warns", "rules", "approval"}
 var notes_help = "✨ Here is the help for **Notes:**\n**Command for Members**\n**->** `/get notename`: get the note with this notename\n**-** #notename: same as /get\n**->** `/notes`: list all saved notes in this chat\n**Command for Admins**\n**->** `/save notename notedata`: saves notedata as a note with name notename, reply to a message or document to save it\n**->** `/clear notename`: clear note with this name\n**->** `/privatenote on/yes/off/no`: whether or not to send the note in PM. Write del besides on/off to delete hashtag message on group.\n**Note**\n **-** Only admins can use This module\n **-** To save a document (like photo, audio, etc.), reply to a document or media then type /save\n **-** Need help for parsing text? Check /markdownhelp\nSave data for future users with notes!\nNotes are great to save random tidbits of information; a phone number, a nice gif, a funny picture - anything!\nAlso you can save a text/document with buttons, you can even save it in here."
 var help = bson.M{"notes": notes_help}
-var help_caption = `
-Hey!, My name is TVS.
+var help_caption = fmt.Sprintf(`
+Hey!, My name is %s.
 Welcome to my telegram bot
 I have lots of handy features.
 So what are you waiting for?
-Click the buttons below.`
+Click the buttons below.`, BOT_NAME)
 
 // The maximum number of characters, upon reaching the number of which the bot starts to stretch the width of the block with buttons:
 var MAX_NUMBER_OF_SYMBOL = 200
